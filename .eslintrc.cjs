89c7c2f216f077093b59366a40930e06900b75dd
Dan Bornstein <danfuzz@milk.com>
2023-04-25 13:24:08 -0700

    Runner and linter.

---
+++ .eslintrc.cjs
@@ -0,0 +1,156 @@
+const plugins = [
+  'jsdoc'
+];
+
+const extendsList = [
+  'eslint:recommended',
+  'plugin:jsdoc/recommended'
+];
+
+const env = {
+  browser: true,
+  es2020: true
+};
+
+const globals = {
+  sampleRate:        'readonly',
+  currentFrame:      'readonly',
+  currentTime:       'readonly',
+  registerProcessor: 'readonly'
+};
+
+const parserOptions = {
+  sourceType: 'module',
+  ecmaVersion: '2022'
+};
+
+const mainRules = {
+  'array-bracket-spacing': 'error',
+  'arrow-parens': 'error',
+  'consistent-return': 'error',
+  'eol-last': 'error',
+  'eqeqeq': 'error',
+  'indent': [
+    'error',
+    2,
+    {
+      'FunctionDeclaration': { 'parameters': 2 },
+      'FunctionExpression':  { 'parameters': 2 },
+      'SwitchCase':          1,
+      'ignoredNodes':        ['TemplateLiteral *']
+    }
+  ],
+  'keyword-spacing': 'error',
+  'max-len': [
+    'error',
+    {
+      'code': 120,
+      'comments': 80,
+      'ignoreRegExpLiterals': true,
+      'ignoreStrings': true,
+      'ignoreTemplateLiterals': true,
+      'ignoreUrls': true,
+      'tabWidth': 8
+    }
+  ],
+  'new-parens': 'error',
+  'no-alert': 'warn',
+  'no-array-constructor': 'error',
+  'no-empty-function': 'error',
+  'no-eval': 'error',
+  'no-extend-native': 'error',
+  'no-fallthrough': ['error', { 'commentPattern': 'fall ?through' }],
+  'no-floating-decimal': 'error',
+  'no-implied-eval': 'error',
+  'no-nested-ternary': 'error',
+  'no-new-func': 'error',
+  'no-new-object': 'error',
+  'no-regex-spaces': 'off',
+  'no-self-assign': ['error', { 'props': true }],
+  'no-shadow': 'error',
+  'no-trailing-spaces': 'error',
+  'no-undef': 'error',
+  'no-unsafe-negation': 'error',
+  'no-unused-vars': [
+    'error',
+    {
+      'vars': 'all',
+      'args': 'all',
+      'varsIgnorePattern': '_unused$',
+      'argsIgnorePattern': '_unused$'
+    }
+  ],
+  'no-var': 'error',
+  'object-curly-spacing': ['error', 'always'],
+  'object-shorthand': ['error', 'always'],
+  'prefer-const': 'error',
+  'prefer-rest-params': 'error',
+  'prefer-spread': 'error',
+  'quotes': [
+    'error',
+    'single',
+    {
+      'avoidEscape':           true,
+      'allowTemplateLiterals': true
+    }
+  ],
+  'semi': ['error', 'always'],
+  'space-before-blocks': ['error', 'always'],
+  'space-before-function-paren': [
+    'error',
+    {
+      'anonymous':  'always',
+      'named':      'never',
+      'asyncArrow': 'always'
+    }
+  ],
+  'symbol-description': 'error'
+};
+
+// Handy links:
+//
+// * JSDoc plugin for ESLint: <https://github.com/gajus/eslint-plugin-jsdoc>
+// * JSDoc: <https://jsdoc.app/>
+// * The Closure type system:
+//   <https://github.com/google/closure-compiler/wiki/Types-in-the-Closure-Type-System>
+const jsdocRules = {
+  'jsdoc/no-multi-asterisks': [
+    'error',
+    {
+      allowWhitespace: true
+    }
+  ],
+  'jsdoc/require-jsdoc': [
+    'warn',
+    {
+      require: {
+        ArrowFunctionExpression: false,
+        ClassDeclaration: true,
+        ClassExpression: false,
+        FunctionDeclaration: false,
+        FunctionExpression: false,
+        MethodDefinition: true
+      }
+    }
+  ],
+  'jsdoc/require-property': [
+    'off'
+  ],
+  'jsdoc/tag-lines': [
+    'warn',
+    'any',
+    {
+      startLines: 1
+    }
+  ]
+};
+
+module.exports = {
+  root: true,
+  plugins,
+  extends: extendsList,
+  env,
+  globals,
+  parserOptions,
+  rules: { ...mainRules, ...jsdocRules }
+};
